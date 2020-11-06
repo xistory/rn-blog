66991f7622aef10f58c0d7aca11b69583f95f950
xistory <ximinseo@hotmail.com>
2020-11-06 12:59:41 +0900

    slight change

--- src/config.js
+++ src/config.js
@@ -13,7 +13,10 @@ const config = {
     USER_POOL_ID: "ap-northeast-2_o1BPrC5l8",
     APP_CLIENT_ID: "4vpk1uab2alcosb9ttb5pdecf1",
     IDENTITY_POOL_ID: "ap-northeast-2:13b49dab-df9e-4280-9f2a-da4f45115ef4"
-  }
+  },
+    social: {
+        FB: "478022039827691"
+    }
 };
 
 
